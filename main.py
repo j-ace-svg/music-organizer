2fdebb98ae44340745e6d80cb052a1318b166a78
John Montgomery <j.ace.svg@gmail.com>
2025-05-06 18:06:59 -0500

    Add python boilerplate

---
+++ main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+import argparse
+
+__TITLE__ = "Music Organizer"
+
+def main(parser=argparse.ArgumentParser()):
+    print(__TITLE__)
+
+    
+    args = parser.parse_args()
+
+if __name__ == "__main__":
+    main()
