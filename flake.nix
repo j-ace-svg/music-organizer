d72674299d5d76d2e5ef9e6b8673e14f1bc1ad22
John Montgomery <j.ace.svg@gmail.com>
2025-05-06 18:06:20 -0500

    Create python development environment

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Pygame development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs-python.url = "github:cachix/nixpkgs-python";
+  };
+
+  outputs = { self, nixpkgs, nixpkgs-python }: 
+    let
+      system = "x86_64-linux";
+
+      pythonVersion = "3.12.2";
+
+
+      pkgs = import nixpkgs { inherit system; };
+      myPython = nixpkgs-python.packages.${system}.${pythonVersion};
+      myPythonWithPackages = myPython.withPackages (ps: with ps; [
+        yt-dlp
+      ]);
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = [
+          myPythonWithPackages
+        ];
+        shellHook = ''
+          python --version
+        '';
+      };
+    };
+}
